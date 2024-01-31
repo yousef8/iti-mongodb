f699490f3e82e1bea5fb2e4db0e197119f24aae1
Yousef Elsayed <yousef4511@outlook.com>
2024-02-01 01:08:08 +0200

    Lab2: Solve 4th question and fix 3rd query

--- lab_2/lab_2_solution.js
+++ lab_2/lab_2_solution.js
@@ -46,7 +46,58 @@ db.users.aggregate([
         from: "products",
         localField: "orders.productsIds",
         foreignField: "_id",
-        as: "Ordered Products"
+        as: "OrderProducts"
     }
-}
+},
+    
+])
+    
+//============= Ahmed Highest Order Price
+db.users.aggregate([
+    {
+        $match: {
+            name: "ahmed"
+        }
+    },
+    {
+        $lookup: {
+            from: "orders",
+            localField: "_id",
+            foreignField: "userId",
+            as: "orders"
+        }
+    },
+    {
+        $unwind: "$orders"
+    },
+    {
+        $lookup: {
+            from: "products",
+            localField: "orders.productsIds",
+            foreignField: "_id",
+            as: "orderProducts"
+        }
+    },
+    {
+        $unwind: "$orderProducts"
+    },
+    {
+        $group: {
+            _id: {
+                userId: "$_id",
+                orderId: "$orders._id"
+            },
+            totalPrice: {
+                $sum: {
+                    $multiply: ["$orderProducts.price", "$orderProducts.quantity"]
+                }
+            }
+        }
+    },
+    {
+        $group: {
+            _id: "$_id.userId",
+            maxOrderPrice: { $max: "$totalPrice" }
+        }
+    }
 ])
